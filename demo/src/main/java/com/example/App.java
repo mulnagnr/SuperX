07ef789595de8353401bc7638a9e57e218b61cdf
mulnagnr <avdhootkadam09@gmail.com>
2025-09-03 18:13:41 +0530

    Javafx - labelDemo

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,46 +1,39 @@
 package com.example;
 
 import javafx.application.Application;
-import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class App extends Application{
     public static void main(String[] args) {
         Application.launch(args);
 
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-
-            
-            Group group = new Group(groupall);
-            Text text = new Text(10,50,"Core2Web");
-            text.setFont(new Font(40));
-            Text 1 = new Text("Avhoot");
-            Group groupall;
-            groupall.getChildren().addAll(text,1);
-
-            Scene scene = new Scene(group,50,50);
-            scene.setFill(Color.AQUAMARINE);
-            scene.setCursor(Cursor.HAND);
-           
-            
-            //stage
-            stage.setTitle("Stage practical");
-            stage.getIcons().add(new Image("file:/D:/Superx2/demo/src/main/resources/images/images.jpeg"));
-            stage.setResizable(false);
-            stage.setX(100);
-            stage.setY(10);
-            stage.setWidth(1000);
-            stage.setHeight(1000);
-            stage.setScene(scene);
-            stage.show();
+    public void start(Stage stage) throws Exception{
+        Label label = new Label("Hello World");
+       // label.setFont(Font.font("ROBOTO", FontWeight.BOLD,30));
+        //label.setRotate(90);
+        
+        //label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        //label.setGraphic(label);
+        //label.setGraphicTextGap(50);
+        
+        Group root = new Group();
+        root.getChildren().add(label);
+        Scene scene = new Scene(root,500,300);
+        stage.setScene(scene);
+        stage.setTitle("This is First Stage");
+        //stage.setResizable(true);
+        // stage.setHeight(800);
+        // stage.setWidth(800);
+       // stage.setFullScreen(true);
+        stage.show();
+        
     }
 }
